9190826125d991faaec6973572eea0b90d0acda5
TeerapongSSD <teerapong.t@ssd.co.th>
2013-08-15 17:13:40 +0700

    Add SampleCode for C#

---
+++ SampleCode/SampleCode/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace SampleCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnum = new System.Windows.Forms.Button();
+            this.btnForeach = new System.Windows.Forms.Button();
+            this.btnYield = new System.Windows.Forms.Button();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnLambda = new System.Windows.Forms.Button();
+            this.btnVar = new System.Windows.Forms.Button();
+            this.btnHashSet = new System.Windows.Forms.Button();
+            this.btnDictionary = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lsvResult = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnEnum
+            // 
+            this.btnEnum.Location = new System.Drawing.Point(19, 24);
+            this.btnEnum.Name = "btnEnum";
+            this.btnEnum.Size = new System.Drawing.Size(146, 23);
+            this.btnEnum.TabIndex = 0;
+            this.btnEnum.Text = "IEnumerable";
+            this.btnEnum.UseVisualStyleBackColor = true;
+            this.btnEnum.Click += new System.EventHandler(this.btnEnum_Click);
+            // 
+            // btnForeach
+            // 
+            this.btnForeach.Location = new System.Drawing.Point(19, 51);
+            this.btnForeach.Name = "btnForeach";
+            this.btnForeach.Size = new System.Drawing.Size(146, 23);
+            this.btnForeach.TabIndex = 1;
+            this.btnForeach.Text = "Foreach";
+            this.btnForeach.UseVisualStyleBackColor = true;
+            this.btnForeach.Click += new System.EventHandler(this.btnForeach_Click);
+            // 
+            // btnYield
+            // 
+            this.btnYield.Location = new System.Drawing.Point(19, 80);
+            this.btnYield.Name = "btnYield";
+            this.btnYield.Size = new System.Drawing.Size(146, 23);
+            this.btnYield.TabIndex = 2;
+            this.btnYield.Text = "Yield";
+            this.btnYield.UseVisualStyleBackColor = true;
+            this.btnYield.Click += new System.EventHandler(this.btnYield_Click);
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(19, 25);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(146, 23);
+            this.btnList.TabIndex = 5;
+            this.btnList.Text = "List<T>";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // btnLambda
+            // 
+            this.btnLambda.Location = new System.Drawing.Point(19, 136);
+            this.btnLambda.Name = "btnLambda";
+            this.btnLambda.Size = new System.Drawing.Size(146, 23);
+            this.btnLambda.TabIndex = 4;
+            this.btnLambda.Text = "Lambda";
+            this.btnLambda.UseVisualStyleBackColor = true;
+            this.btnLambda.Click += new System.EventHandler(this.btnLambda_Click);
+            // 
+            // btnVar
+            // 
+            this.btnVar.Location = new System.Drawing.Point(19, 108);
+            this.btnVar.Name = "btnVar";
+            this.btnVar.Size = new System.Drawing.Size(146, 23);
+            this.btnVar.TabIndex = 3;
+            this.btnVar.Text = "Var";
+            this.btnVar.UseVisualStyleBackColor = true;
+            this.btnVar.Click += new System.EventHandler(this.btnVar_Click);
+            // 
+            // btnHashSet
+            // 
+            this.btnHashSet.Location = new System.Drawing.Point(19, 50);
+            this.btnHashSet.Name = "btnHashSet";
+            this.btnHashSet.Size = new System.Drawing.Size(146, 23);
+            this.btnHashSet.TabIndex = 6;
+            this.btnHashSet.Text = "HashSet<T>";
+            this.btnHashSet.UseVisualStyleBackColor = true;
+            this.btnHashSet.Click += new System.EventHandler(this.btnHashSet_Click);
+            // 
+            // btnDictionary
+            // 
+            this.btnDictionary.Location = new System.Drawing.Point(19, 75);
+            this.btnDictionary.Name = "btnDictionary";
+            this.btnDictionary.Size = new System.Drawing.Size(146, 23);
+            this.btnDictionary.TabIndex = 7;
+            this.btnDictionary.Text = "Dictionary<TKey,TValue>";
+            this.btnDictionary.UseVisualStyleBackColor = true;
+            this.btnDictionary.Click += new System.EventHandler(this.btnDictionary_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnEnum);
+            this.groupBox1.Controls.Add(this.btnForeach);
+            this.groupBox1.Controls.Add(this.btnLambda);
+            this.groupBox1.Controls.Add(this.btnYield);
+            this.groupBox1.Controls.Add(this.btnVar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(186, 173);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Basic Code";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnList);
+            this.groupBox2.Controls.Add(this.btnHashSet);
+            this.groupBox2.Controls.Add(this.btnDictionary);
+            this.groupBox2.Location = new System.Drawing.Point(235, 7);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(187, 118);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Generic Collections";
+            // 
+            // lsvResult
+            // 
+            this.lsvResult.FormattingEnabled = true;
+            this.lsvResult.Location = new System.Drawing.Point(19, 18);
+            this.lsvResult.Name = "lsvResult";
+            this.lsvResult.Size = new System.Drawing.Size(146, 69);
+            this.lsvResult.TabIndex = 8;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lsvResult);
+            this.groupBox3.Location = new System.Drawing.Point(235, 131);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(187, 100);
+            this.groupBox3.TabIndex = 11;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 242);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sample Code C#";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnum;
+        private System.Windows.Forms.Button btnForeach;
+        private System.Windows.Forms.Button btnYield;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnLambda;
+        private System.Windows.Forms.Button btnVar;
+        private System.Windows.Forms.Button btnHashSet;
+        private System.Windows.Forms.Button btnDictionary;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox lsvResult;
+        private System.Windows.Forms.GroupBox groupBox3;
+    }
+}
+
---
+++ SampleCode/SampleCode/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SampleCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+#region SampleCode
+
+        private void btnEnum_Click(object sender, EventArgs e)
+        {
+            clearList();
+            IEnumerable<int> result = from value in Enumerable.Range(1, 3)
+                                      select value;
+            foreach (var n in result)
+            {
+                lsvResult.Items.Add(n);
+            }
+        }         
+
+        private void btnForeach_Click(object sender, EventArgs e)
+        {
+            clearList();
+            string[] testName = { "Teerapong", "Tatiyaprapa" };
+            foreach (var n in testName)
+            {
+                lsvResult.Items.Add(n);
+            }
+        }
+
+        private void btnYield_Click(object sender, EventArgs e)
+        {
+            clearList();
+            foreach (var n in testYield(2, 10))
+            {
+                lsvResult.Items.Add(n);            
+            }
+        }
+
+        public static IEnumerable<int> testYield(int first, int second)
+        {
+            int count = 0;
+            int result = 1;
+            while (count < second)
+            {
+                result *= first;
+                count++;
+                yield return result;
+            }
+        }
+
+        private void btnVar_Click(object sender, EventArgs e)
+        {
+            clearList();
+            string[] testVar = { "Teerapong", "Tatiyaprapa", "SSD", "OnTheRoAd" };
+            var varQuery = from q in testVar
+                            where q.ToUpper().ToLower().Contains("a")
+                            select q;
+            foreach (var n in varQuery)
+            {
+                lsvResult.Items.Add(n);
+            }
+            lsvResult.Items.Add(ReturnValue(10).ToString());
+        }
+
+        public int ReturnValue(int var) // Button Var
+        {
+            var a = 10;
+            return a * var;
+        }
+
+        private void btnLambda_Click(object sender, EventArgs e)
+        {
+            clearList();
+            var nuturalNumbers = Enumerable.Range(1, 101);
+            string[] names = { "SSD", "OnTheRoad", "Terapong" };
+            IEnumerable<string> query = names.Where(n => n.ToUpper().ToLower().Contains("o"))
+                                             .OrderBy(n => n.Length)
+                                             .Select(n => n.ToUpper());
+            foreach (var n in query)
+            {
+                lsvResult.Items.Add(n);
+            }
+        }
+
+#endregion
+
+#region Generic Collection
+
+
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            clearList();
+            List<string> testList = new List<string>();
+            testList.Add("Teerapong");
+            testList.Add("Tatiyaprapa");
+            testList.Add("SSD");
+
+            List<int> testList1 = new List<int>();
+            testList1.Add(1);
+            testList1.Add(2);
+            testList1.Add(3);
+
+            foreach (var n in testList)
+            {
+            lsvResult.Items.Add(n);
+            }
+        } 
+
+        private void btnHashSet_Click(object sender, EventArgs e)
+        {
+            clearList();
+
+            var letters = new HashSet<char>("teerapong");
+            string result = "";
+
+            foreach (char c in letters) result += c;
+            lsvResult.Items.Add(result); result = "";
+
+            lsvResult.Items.Add(letters.Contains('t')); 
+            lsvResult.Items.Add(letters.Contains('z'));    
+
+            letters = new HashSet<char>("teerapong");
+            letters.IntersectWith("aeiou");
+            foreach (char c in letters) result += c;
+            lsvResult.Items.Add(result); result = "";
+
+            letters = new HashSet<char>("teerapong");
+            letters.ExceptWith("aeiou");
+            foreach (char c in letters) result += c;
+            lsvResult.Items.Add(result); result = "";
+
+            letters = new HashSet<char>("teerapong");
+            letters.SymmetricExceptWith("teerapongs" );
+            foreach (char c in letters) result += c;
+            lsvResult.Items.Add(result); result = "";
+
+            HashSet<int> first = new HashSet<int>();
+            HashSet<int> second = new HashSet<int>();
+            for (var i = 1; i <= 5; i++)
+            {
+                first.Add(5 * i);
+            }
+            for (var j = 1; j <= 5; j++)
+            {
+                second.Add(10 * j);
+            }
+            lsvResult.Items.Add("The table of 5 is:");
+            foreach (var n in first)
+            {
+                lsvResult.Items.Add(n);
+            }
+            lsvResult.Items.Add("The table of 10 is:");
+            foreach (var n in second)
+            {
+                lsvResult.Items.Add(n);
+            }
+        }
+
+        private void btnDictionary_Click(object sender, EventArgs e)
+        {
+            clearList();
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            dict.Add("Teerapong", 17);
+            dict.Add("SSD", 5);
+
+            foreach (KeyValuePair<string, int> n in dict)
+            {
+                lsvResult.Items.Add(n.Key.ToString());
+            }
+            if (dict.ContainsKey("Teerapong"))
+            {
+                var value = dict["Teerapong"];
+                lsvResult.Items.Add(value);
+            }      
+            if (!dict.ContainsKey("OnTheRoad"))
+            {
+                lsvResult.Items.Add(false);
+            }      
+        }
+
+#endregion
+
+
+        private void clearList()
+        {
+            lsvResult.Items.Clear();
+        }    
+
+    }     
+
+
+}
